a1164c4151acb47455bc52680b860cf102660e26
gergo <pap133@gmail.com>
2018-12-13 10:32:45 +0100

    category enum added

--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -5,5 +5,12 @@ export class AppModel {
     public category?: string,
     public text?: string
   ) {
+    Object.keys(category);
   }
+}
+export enum category {
+  BILLING = 'BILLING',
+  HOUSE = 'HOUSE',
+  SHOPPING = 'SHOPPING',
+  TRAVEL = 'TRAVEL'
 }
